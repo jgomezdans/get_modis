48b420da3d84c35c7c7a430d8036b0eb31337dca
Jose Gomez-Dans <jgomezdans@gmail.com>
2016-09-06 17:09:15 +0100

    The changes so far have more to do with the code being PEP-8 compliant, or as far as I'm aware of it. Username and password downloads work now, but I have had to add the requests package, as I can't figure out how to do the authentication using urllib or urllib2.

--- get_modis.py
+++ get_modis.py
@@ -16,10 +16,23 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with brdf_filter.  If not, see <http://www.gnu.org/licenses/>.
+import optparse
+import os
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+import time
+import calendar
+import logging
+import sys
+import fnmatch
+import requests
+
 
 __author__ = "J Gomez-Dans"
 __copyright__ = "Copyright 2013-2016 J Gomez-Dans"
-__version__ = "1.0 (09.03.2015)"
+__version__ = "1.1"
 __license__ = "GPLv3"
 __email__ = "j.gomez-dans@ucl.ac.uk"
 
@@ -65,18 +78,6 @@ AUTHOR
     See also http://github.com/jgomezdans/get_modis/
 
 """
-import optparse
-import os
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-import time
-import calendar
-import logging
-import sys
-import fnmatch
-import requests
 
 LOG = logging.getLogger( __name__ )
 OUT_HDLR = logging.StreamHandler( sys.stdout )
@@ -85,19 +86,19 @@ OUT_HDLR.setLevel( logging.INFO )
 LOG.addHandler( OUT_HDLR )
 LOG.setLevel( logging.INFO )
 
-HEADERS = { 'User-Agent' : 'get_modis Python 1.3.0' }
+HEADERS = { 'User-Agent' : 'get_modis Python %s' % __version__ }
 
 CHUNKS = 65536
-def return_url ( url ):
 
+
+def return_url(url):
     the_day_today = time.asctime().split()[0]
-    the_hour_now = int( time.asctime().split()[3].split(":")[0] )
+    the_hour_now = int(time.asctime().split()[3].split(":")[0])
     if the_day_today == "Wed" and 14 <= the_hour_now <= 17:
-        LOG.info ( "Sleeping for %d hours... Yawn!" % ( 18 - the_hour_now) )
-        time.sleep ( 60*60*( 18-the_hour_now) )
-        #LOG.info ( "Sleeping for %d hours... Yawn!" % ( 18 - the_hour_now) )
+        LOG.info("Sleeping for %d hours... Yawn!" % (18 - the_hour_now))
+        time.sleep(60 * 60 * (18 - the_hour_now))
 
-    req = urllib2.Request ( "%s" % ( url ), None, HEADERS)
+    req = urllib2.Request("%s" % (url), None, HEADERS)
     html = urllib2.urlopen(req).readlines()
     return html
 
@@ -131,46 +132,45 @@ def parse_modis_dates ( url, dates, product, out_dir, ruff=False ):
     """
     if ruff:
         product = product.split(".")[0]
-        already_here = fnmatch.filter ( os.listdir ( out_dir ), "%s*hdf" % product )
-        already_here_dates = [ x.split(".")[-5][1:] \
-            for x in already_here ]
+        already_here = fnmatch.filter(os.listdir(out_dir),
+                                      "%s*hdf" % product)
+        already_here_dates = [x.split(".")[-5][1:]
+                              for x in already_here]
 
-    #req = urllib2.Request ( "%s" % ( url ), None, HEADERS)
-    #html = urllib2.urlopen(req).readlines()
-    html = return_url ( url )
+    html = return_url(url)
 
     available_dates = []
     for line in html:
 
-        if line.decode().find ( "href" ) >= 0 and line.decode().find ( "[DIR]" ) >= 0:
+        if line.decode().find("href") >= 0 and \
+                        line.decode().find("[DIR]") >= 0:
             # Points to a directory
             the_date = line.decode().split('href="')[1].split('"')[0].strip("/")
-
             if ruff:
                 try:
-                    modis_date = time.strftime( "%Y%j", time.strptime( \
-                        the_date, "%Y.%m.%d") )
+                    modis_date = time.strftime("%Y%j",
+                                               time.strptime(the_date,
+                                                             "%Y.%m.%d"))
                 except ValueError:
                     continue
                 if modis_date in already_here_dates:
                     continue
                 else:
-                    available_dates.append ( the_date )
+                    available_dates.append(the_date)
             else:
-                available_dates.append ( the_date )
+                available_dates.append(the_date)
 
-
-
-    dates = set ( dates )
-    available_dates = set ( available_dates )
-    suitable_dates = list( dates.intersection( available_dates ) )
+    dates = set(dates)
+    available_dates = set(available_dates)
+    suitable_dates = list(dates.intersection(available_dates))
     suitable_dates.sort()
     return suitable_dates
 
 
-def get_modisfiles ( username, password, platform, product, year, tile, proxy, \
-    doy_start=1, doy_end = -1,  \
-    base_url="http://e4ftl01.cr.usgs.gov", out_dir=".", ruff=False, verbose=False ):
+def get_modisfiles(username, password, platform, product, year, tile, proxy,
+                   doy_start=1, doy_end=-1,
+                   base_url="http://e4ftl01.cr.usgs.gov", out_dir=".",
+                   ruff=False, verbose=False):
 
     """Download MODIS products for a given tile, year & period of interest
 
@@ -187,6 +187,10 @@ def get_modisfiles ( username, password, platform, product, year, tile, proxy, \
 
     Parameters
     ----------
+    username: str
+        The EarthData username string
+    password: str
+        The EarthData username string
     platform: str
         One of three: MOLA, MOLT MOTA
     product: str
@@ -219,67 +223,51 @@ def get_modisfiles ( username, password, platform, product, year, tile, proxy, \
     """
 
     if proxy is not None:
-        proxy = urllib2.ProxyHandler( proxy )
-        opener = urllib2.build_opener( proxy )
-        urllib2.install_opener( opener )
+        proxy = urllib2.ProxyHandler(proxy)
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
 
-    if not os.path.exists ( out_dir ):
+    if not os.path.exists(out_dir):
         if verbose:
-            LOG.info("Creating outupt dir %s" % out_dir )
-        os.makedirs ( out_dir )
+            LOG.info("Creating outupt dir %s" % out_dir)
+        os.makedirs(out_dir)
     if doy_end == -1:
-        if calendar.isleap ( year ):
+        if calendar.isleap(year):
             doy_end = 367
         else:
             doy_end = 366
 
-    dates = [time.strftime("%Y.%m.%d", time.strptime( "%d/%d" % ( i, year ), \
-            "%j/%Y"))  for i in range(doy_start, doy_end )]
-    url = "%s/%s/%s/" % ( base_url, platform, product )
-    dates = parse_modis_dates ( url, dates, product, out_dir, ruff=ruff )
+    dates = [time.strftime("%Y.%m.%d", time.strptime("%d/%d" % (i, year),
+                                                     "%j/%Y")) for i in
+             range(doy_start, doy_end)]
+    url = "%s/%s/%s/" % (base_url, platform, product)
+    dates = parse_modis_dates(url, dates, product, out_dir, ruff=ruff)
     them_urls = []
     for date in dates:
-        #req = urllib2.Request ( "%s/%s" % ( url, date), None, HEADERS )
-        try:
-            #html = return_url ( "%s/%s" % ( url, date ) )
-            r = requests.get ( "%s/%s" % ( url, date), verify=False )
-            #html = urllib2.urlopen(req).readlines()
-            for line in r.text.split("\n"):
-                if line.decode().find( tile ) >=0  and line.decode().find(".hdf") >= 0 and \
-                    line.decode().find(".hdf.xml") < 0:
+        r = requests.get("%s/%s" % (url, date), verify=False)
+        for line in r.text.split("\n"):
+            if line.decode().find(tile) >= 0:
+                if line.decode().find(
+                        ".hdf") >= 0 > line.decode().find(".hdf.xml"):
                     fname = line.decode().split("href=")[1].split(">")[0].strip('"')
-                    
-                    #req = urllib2.Request ( "%s/%s/%s" % ( url, date, fname), \
-                    #    None, HEADERS )
-                    download = False
-                    if not os.path.exists ( os.path.join( out_dir, fname ) ):
-                        # File not present, download
-                        download = True
 
-                    if download:
-                        them_urls.append ( "%s/%s/%s" % ( url, date, fname) )
+                    if not os.path.exists(os.path.join(out_dir, fname)):
+                        them_urls.append("%s/%s/%s" % (url, date, fname))
                     else:
                         if verbose:
-                            LOG.info ("File %s already present. Skipping" % \
-                                fname )
-            
-        except urllib2.URLError:
-            LOG.info("Could not find data for %s(%s) for %s" % ( product, \
-                platform, date ))
-    
+                            LOG.info("File %s already present. Skipping" % fname)
+
     with requests.Session() as s:
-        s.auth=(username, password)
+        s.auth = (username, password)
         for the_url in them_urls:
-            r1 = s.request('get', the_url )
-            r = s.get (r1.url, stream=True )
+            r1 = s.request('get', the_url)
+            r = s.get(r1.url, stream=True)
             if not r.ok:
                 raise IOError("Can't start download... [%s]" % fname)
             file_size = int(r.headers['content-length'])
-            LOG.info ("Starting download on %s(%d bytes) ..." % 
-                      (os.path.join ( out_dir,fname ), file_size))
+            LOG.info("Starting download on %s(%d bytes) ..." %
+                     (os.path.join(out_dir, fname), file_size))
             with open(os.path.join(out_dir, fname), 'wb') as fp:
-                cntr = 0
-                dload = 0
                 for chunk in r.iter_content(chunk_size=CHUNKS):
                     if chunk:
                         fp.write(chunk)
@@ -292,50 +280,51 @@ def get_modisfiles ( username, password, platform, product, year, tile, proxy, \
 
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), \
-        usage=globals()['__doc__'])
-    parser.add_option ('-u', '--username', action="store", dest="username",
-                       help="EarthData username")
-    parser.add_option ('-P', '--password', action="store", dest="password",
-                       help="EarthData password")
-    parser.add_option ('-v', '--verbose', action='store_true', \
-        default=False, help='verbose output')
-    parser.add_option ('-s', '--platform', action='store', dest="platform", \
-        type=str, help='Platform type: MOLA, MOLT or MOTA' )
-    parser.add_option ('-p', '--product', action='store', dest="product", \
-        type=str, help="MODIS product name with collection tag at the end " + \
-            "(e.g. MOD09GA.005)" )
-    parser.add_option ('-t', '--tile', action="store", dest="tile", \
-        type=str, help="Required tile (h17v04, for example)")
-    parser.add_option ( "-y", "--year", action="store", dest="year", \
-        type=int, help="Year of interest" )
-    parser.add_option('-o', '--output', action="store", dest="dir_out", \
-        default=".", type=str, help="Output directory" )
-    parser.add_option('-b', '--begin', action="store", dest="doy_start", \
-        default=1, type=int, help="Starting day of year (DoY)" )
-    parser.add_option('-e', '--end', action="store", dest="doy_end", \
-        type=int, default=-1, help="Ending day of year (DoY)" )
-    parser.add_option('-r', '--proxy', action="store", dest="proxy", \
-        type=str, default=None, help="HTTP proxy URL" )
-    parser.add_option('-q', '--quick', action="store_true", dest="quick", \
-        default=False, help="Quick check to see whether files are present" )
+    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(),
+                                   usage=globals()['__doc__'])
+    parser.add_option('-u', '--username', action="store", dest="username",
+                      help="EarthData username")
+    parser.add_option('-P', '--password', action="store", dest="password",
+                      help="EarthData password")
+    parser.add_option('-v', '--verbose', action='store_true',
+                      default=False, help='verbose output')
+    parser.add_option('-s', '--platform', action='store', dest="platform",
+                      type=str, help='Platform type: MOLA, MOLT or MOTA')
+    parser.add_option('-p', '--product', action='store', dest="product",
+                      type=str,
+                      help="MODIS product name with collection tag at the end " +
+                           "(e.g. MOD09GA.005)")
+    parser.add_option('-t', '--tile', action="store", dest="tile",
+                      type=str, help="Required tile (h17v04, for example)")
+    parser.add_option("-y", "--year", action="store", dest="year",
+                      type=int, help="Year of interest")
+    parser.add_option('-o', '--output', action="store", dest="dir_out",
+                      default=".", type=str, help="Output directory")
+    parser.add_option('-b', '--begin', action="store", dest="doy_start",
+                      default=1, type=int, help="Starting day of year (DoY)")
+    parser.add_option('-e', '--end', action="store", dest="doy_end",
+                      type=int, default=-1, help="Ending day of year (DoY)")
+    parser.add_option('-r', '--proxy', action="store", dest="proxy",
+                      type=str, default=None, help="HTTP proxy URL")
+    parser.add_option('-q', '--quick', action="store_true", dest="quick",
+                      default=False,
+                      help="Quick check to see whether files are present")
     (options, args) = parser.parse_args()
     if 'username' not in options.__dict__:
         parser.error("You need to provide a username! Sgrunt!")
     if 'password' not in options.__dict__:
         parser.error("You need to provide a password! Sgrunt!")
-    if not ( options.platform in [ "MOLA", "MOTA", "MOLT" ] ) :
-        LOG.fatal ("`platform` has to be one of MOLA, MOTA, MOLT")
+    if not (options.platform in ["MOLA", "MOTA", "MOLT"]):
+        LOG.fatal("`platform` has to be one of MOLA, MOTA, MOLT")
         sys.exit(-1)
     if options.proxy is not None:
-        PROXY = { 'http': options.proxy }
+        PROXY = {'http': options.proxy}
     else:
         PROXY = None
 
-
-
-    get_modisfiles ( options.username, options.password, options.platform, options.product, options.year, \
-            options.tile, PROXY, \
-            doy_start=options.doy_start, doy_end=options.doy_end, \
-            out_dir=options.dir_out, \
-            verbose=options.verbose, ruff=options.quick )
+    get_modisfiles(options.username, options.password, options.platform,
+                   options.product, options.year,
+                   options.tile, PROXY,
+                   doy_start=options.doy_start, doy_end=options.doy_end,
+                   out_dir=options.dir_out,
+                   verbose=options.verbose, ruff=options.quick)
